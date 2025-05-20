089eb3c3aa149a906930214b2e67d220132f1599
Khislat <khislatolimov29@gmail.com>
2025-05-20 07:27:52 -0700

    feat: publish AboutSection

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import withLayoutMain from "../libs/components/layout/LayoutHome";
 import { Stack } from "@mui/material";
 import useDeviceDetect from "../libs/hooks/useDeviceDetect";
 import HeroSection from "../libs/components/homepage/HeroSection";
-
+import AboutSection from "../libs/components/homepage/AboutSection";
 
 const Home: NextPage = () => {
 	const device = useDeviceDetect();
@@ -12,9 +12,12 @@ const Home: NextPage = () => {
 	if (device === "mobile") {
 		return <Stack className={"home-page"}>joijjopijoipjpojpojopij</Stack>;
 	} else {
-		return <Stack className={"home-page"}>
-			<HeroSection  />
-		</Stack>;
+		return (
+			<Stack className={"home-page"}>
+				<HeroSection />
+				<AboutSection />
+			</Stack>
+		);
 	}
 };
 
