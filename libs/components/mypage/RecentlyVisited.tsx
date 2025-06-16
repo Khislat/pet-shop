aa475683ca23337e318fffd7eb08463e8a88d5e1
Khislat <khislatolimov29@gmail.com>
2025-06-16 08:24:35 -0700

    feat: develop RecentlyVisited section

--- libs/components/mypage/RecentlyVisited.tsx
+++ libs/components/mypage/RecentlyVisited.tsx
@@ -3,11 +3,13 @@ import { NextPage } from "next";
 import useDeviceDetect from "../../hooks/useDeviceDetect";
 import { Pagination, Stack, Typography } from "@mui/material";
 import { T } from "../../types/common";
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import { GET_VISITED } from "../../../apollo/user/query";
 import { Product } from "../../types/product/product";
 import ProductCards from "./ProductCards";
-import ShopCard from "../shoppage/ShopCard";
+import { LIKE_TARGET_PRODUCT } from "../../../apollo/user/mutation";
+import { Messages } from "../../config";
+import { sweetMixinErrorAlert } from "../../sweetAlert";
 
 const RecentlyVisited: NextPage = () => {
 	const device = useDeviceDetect();
@@ -16,6 +18,8 @@ const RecentlyVisited: NextPage = () => {
 	const [searchVisited, setSearchVisited] = useState<T>({ page: 1, limit: 6 });
 
 	/** APOLLO REQUESTS **/
+	const [likeTargetProduct] = useMutation(LIKE_TARGET_PRODUCT);
+
 	const {
 		loading: getVisitedLoading,
 		data: getVisitedData,
@@ -27,7 +31,7 @@ const RecentlyVisited: NextPage = () => {
 		notifyOnNetworkStatusChange: true,
 		onCompleted: (data: T) => {
 			setRecentlyVisited(data?.getVisited?.list);
-			setTotal(data?.getFavorites?.metaCounter[0]?.total || 0);
+			setTotal(data?.getVisited?.metaCounter[0]?.total ?? 0);
 		},
 	});
 
@@ -36,8 +40,22 @@ const RecentlyVisited: NextPage = () => {
 		setSearchVisited({ ...searchVisited, page: value });
 	};
 
+	const likeProductHandler = async (user: T, id: string) => {
+		try {
+			if (!id) return;
+			if (!user._id) throw new Error(Messages.error2);
+
+			await likeTargetProduct({ variables: { input: id } });
+
+			await getVisitedRefetch({ input: searchVisited });
+		} catch (err: any) {
+			console.log("ERROR, likeProductHandler:", err.message);
+			sweetMixinErrorAlert(err.message).then();
+		}
+	};
+
 	if (device === "mobile") {
-		return <div>PETSHOP MY FAVORITES MOBILE</div>;
+		return <div>PETSHOP MY RECEANTL VISITED MOBILE</div>;
 	} else {
 		return (
 			<div id="my-favorites-page">
@@ -50,20 +68,22 @@ const RecentlyVisited: NextPage = () => {
 					</Stack>
 				</Stack>
 				<Stack className="favorites-list-box">
-					{recentlyVisited.length === 0 ? (
+					{recentlyVisited.length ? (
+						recentlyVisited?.map((product: Product) => {
+							return (
+								<ProductCards
+									product={product}
+									myFavorites={true}
+									recentlyVisited={true}
+									likeProductHandler={likeProductHandler}
+								/>
+							);
+						})
+					) : (
 						<div className={"no-data"}>
 							<img src="/img/icons/icoAlert.svg" alt="" />
-							<p>No Product found!</p>
+							<p>No Visited Products!</p>
 						</div>
-					) : (
-						recentlyVisited.map((product: Product) => (
-							<ProductCards
-								product={product}
-								recentlyVisited={true}
-								myFavorites={false}
-								likeProductHandler={undefined}
-							/>
-						))
 					)}
 				</Stack>
 				{recentlyVisited.length !== 0 && (
